348e1dab552057b7ecec77c52adeecc28fbf11a7
intunewithnature <ampheatt@gmail.com>
2025-11-18 12:40:44 -0500

    Refactor NixOS configurations for vps and test-server

--- flake.nix
+++ flake.nix
@@ -6,12 +6,23 @@
   };
 
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.vps = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        ./hardware-configuration.nix
-      ];
+    nixosConfigurations = {
+      vps = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./hardware-vps.nix
+        ];
+      };
+
+      test-server = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./hardware-test-server.nix
+        ];
+      };
     };
   };
 }
+EOF
