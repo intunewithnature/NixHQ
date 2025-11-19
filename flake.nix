e33839634dd205d90554a03ef66edc851e985149
intunewithnature <ampheatt@gmail.com>
2025-11-18 19:55:17 -0500

    Add Caddy modules to VPS and test server configurations
    Added Caddy modules for production and staging environments.

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "NixHQ VPS NixOS config";
 
   inputs = {
+    # Pin to 25.05 release
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
@@ -9,18 +10,22 @@
     nixosConfigurations = {
       vps = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+
         modules = [
           ./configuration.nix
           ./hardware-vps.nix
+          ./modules/caddy-prod.nix
           { networking.hostName = "impious-vps"; }
         ];
       };
 
       test-server = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+
         modules = [
           ./configuration.nix
           ./hardware-test-server.nix
+          ./modules/caddy-staging.nix
           { networking.hostName = "test-server"; }
         ];
       };
